1f83bf619aa9d2ae48c0892fcad9a4610947f522
Jottahdevjr <juniorjonathan164@gmail.com>
2026-09-10 11:34:15 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,95 @@
+    const API_URL = 'http://localhost:3000/produto';
+
+    const carregarProdutos = async () =>{
+        try{
+            const res = await fetch(API_URL)
+            const produtos = await res.json()
+
+        const conteiner = document.getElementById('listaProdutos')
+        conteiner.innerHTML = ""
+
+        produtos.forEach(p => {  
+            const card = document.createElement('div')
+            card.className = 'card-produto'
+
+            card.innerHTML = `
+            <h3>${p.nome}</h3>
+            <p>Preço: R$ ${p.preco}</p>
+            <p>Categoria: ${p.categoria}</p>     
+            <p>Quantidade: ${p.emEstoque}</p>
+
+            <button id="Editar" onclick ="editarProduto('${p._id}', '${p.nome}', ${p.preco}, ${p.emEstoque})">Editar</button>
+            <button id="Editar" onclick="deletarProduto('${p._id}')">
+            Excluir
+            </button>
+            `;
+
+            conteiner.appendChild(card)
+        });
+            } catch (error) {
+            console.error('Erro ao buscar produtos:', error);
+        }};
+
+    document.getElementById('formProduto').addEventListener('submit', async (e)=> {
+        e.preventDefault();
+
+        const novoProduto ={
+            nome : document.getElementById('nome').value,
+            preco : Number(document.getElementById('preco').value),
+            categoria : document.getElementById('categoria').value,
+            emEstoque : Number(document.getElementById('quantidade').value)
+        }
+
+        try{
+            await fetch(API_URL,{
+                method:'POST',
+                headers:{ 'Content-Type': 'application/json'},
+                body:JSON.stringify(novoProduto)
+            })
+        }catch(error){
+            console.log("Erro ao enviar o novo produto", error)
+        }
+        e.target.reset();
+        carregarProdutos();  
+        })
+
+        const editarProduto = async (id, nomeAtual, preçoAtual, quantidadeAtual) =>{
+            const novoNome = prompt("Novo nome do produto:", nomeAtual)
+            const novoPreco = prompt("Novo preço do produto:", preçoAtual)
+            const novaQuantidade = prompt("Nova quantidade do produto:", quantidadeAtual)
+
+        if(!novoNome || !novoPreco || !novaQuantidade){
+            alert("Preencha todos os campos para prosseguir")
+            return;
+        }else if(novoNome && novoPreco && novaQuantidade){
+            const dadosAtualizados = {
+                nome : novoNome,
+                preco : Number(novoPreco),
+                emEstoque : novaQuantidade
+            }
+            try{
+            await fetch(`${API_URL}/${id}`, {
+                method:'PUT',
+                headers:{ 'Content-Type': 'application/json'},
+                body:JSON.stringify(dadosAtualizados),
+            })
+
+            carregarProdutos()
+
+        }catch(error){
+            console.error("Erro ao atualizar o produto:", error);
+        }
+        }}
+
+        const deletarProduto = async (id)=>{
+            if(confirm("Deseja realmente apagar")){
+                try{
+                await fetch(`${API_URL}/${id}`,{
+                    method:'DELETE'
+                });
+
+                carregarProdutos()
+            }catch(error){
+                console.error("Erro ao deletar o produto:", error);
+            }}
+    }
---
+++ servidor/server.js
@@ -0,0 +1,85 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+const ConectarBD = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Banco de Dados conectado com sucesso!')
+    } catch (Error){
+        console.log('Erro ao conectar ao banco de dados', Error)
+    }
+}
+
+ConectarBD()
+
+const produtoSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    preco: {
+        type: Number,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    emEstoque: {
+        type: Number,
+        required: true
+    }
+});
+
+const Produto = mongoose.model('Produto', produtoSchema);
+
+app.get('/produto', async (req,res) =>{
+    try{
+    const produtos = await Produto.find()
+    res.json(produtos)
+    }catch(error){
+        res.status(500).json({mensagem:"Erro ao encontrar o produto"})
+    }
+})
+
+app.post('/produto', async (req,res) => {
+    try{
+        const novoProduto = await Produto.create(req.body);
+        res.status(201).json(novoProduto);
+    } catch (error){
+        res.status(400).json({mensagem: "Erro ao cadastrar produto", erro: error});
+    }
+})
+
+app.put('/produto/:id', async (req,res) =>{
+    try{
+        const produtoAtualizado = await Produto.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.json(produtoAtualizado)
+    }catch (error) {
+        res.status(400).json({mensagem:"Erro ao atualizar o produto"})
+    }
+})
+
+app.delete('/produto/:id', async (req,res) =>{
+    try{
+       await Produto.findByIdAndDelete(req.params.id);
+       res.json({mensagem:"Produto deletado com sucesso!"})
+    }catch(error){
+        res.status(400).json({mensagem:"Erro ao deletar o produto"})
+    }
+})
+
+
+app.listen(port,() =>{
+    console.log('Servidor rodando')
+});
